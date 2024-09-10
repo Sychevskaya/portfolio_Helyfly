ef89640a2f15a593110cb8c0a476085c82411895
Sychevskaya <liza.sychevskaya@bk.ru>
2024-09-10 23:41:14 +0300

    Helyfly

---
+++ js/slick.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.main-slider_2').slick({
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
+
+
+$(document).ready(function(){
+    $('.main-slider_3').slick({
+        dots: true,
+        slidesToShow: 1
+    });
+  });
+
